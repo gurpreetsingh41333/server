0e9f58002a98b926374b07fe48ac178bf54592ef
Sunaina Passi <sunaina.passi4@gmail.com>
2018-06-10 02:02:57 +0530

    #master response chnaged

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({ HI: 'There' });
+  res.send("Good job Buddy");
 });
 
 const PORT = process.env.PORT || 5000;
